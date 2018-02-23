3a4f4432be8882c28122420590fd658a8eb71bf3
Curt Barnard <luciddr34m3r@gmail.com>
2018-02-23 15:57:23 -0500

    Bugfix

--- logparsers/TFHttpLog.py
+++ logparsers/TFHttpLog.py
@@ -23,6 +23,21 @@ class TFHttpLog:
 
         self.reducer = TFGenericLog(logfile, ["80:tcp", "8080:tcp"], apiKey)
 
+        # quietLogs are logs that have had noise removed
+        self.quietLogs = self.reducer.quietLogs
+
+        # noisyLogs are logs that we think are noise
+        self.noisyLogs = self.reducer.noisyLogs
+
+        self.parsedLog = self.reducer.parsedLog
+        self.unhandledLogs = self.reducer.unhandledLogs
+
+        # alertLogs are logs where we think a noisy actor managed to do something bad
+        # For example, if someone has a successful auth attempt, but they
+        # are known to be brute forcing ssh servers, they may have successfully broken in
+        self.alertLogs = []
+
+
     ################################
     # Description:
     #   Print the reduced log file
